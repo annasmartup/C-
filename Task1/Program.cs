a783c405e3ed5964bea69edb743617d130ac12f7
Anna_skazka <smartupfit@yandex.ru>
2024-02-17 02:19:36 +1000

    Пишем код в файле програм

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello world");
+
+using System.ComponentModel;
+
+int x = 5;
+Console.WriteLine(x);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
